42cd17f19cffe3a447990cd0b3531bf96e8edd2d
enfldia <enfldia12@naver.com>
2023-10-04 21:40:12 +0900

    1004

--- Hello/src/homework/a1004/A36.java
+++ Hello/src/homework/a1004/A36.java
@@ -1,7 +1,36 @@
 package homework.a1004;
 
+import java.util.Scanner;
+
 public class A36 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("첫번째 주사위 눈의 갯수를 입력하세요.");
+        int n = scanner.nextInt();
+        while (true) {
+            if ((n == 1) | (n <= 10)) {
+                break;
+            } else {
+                System.out.println("10이하의 자연수를 입력하세요.");
+                n = scanner.nextInt();
+            }
+        }
+        System.out.println("두번째 주사위 눈의 갯수를 입력하세요.");
+        int m = scanner.nextInt();
+        while (true) {
+            if ((m == 1) | (m <= 10)) {
+                break;
+            } else {
+                System.out.println("10이하의 자연수를 입력하세요.");
+                m = scanner.nextInt();
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= m; j++) {
+                System.out.println("(" + i + "," + j + ")");
+            }
+        }
+        scanner.close();
     }
 }
---
+++ Hello/src/homework/a1004/A37.java
@@ -0,0 +1,20 @@
+package homework.a1004;
+
+import java.util.Scanner;
+
+public class A37 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("사각형의 변의 길이를 입력하세요.");
+        int n = scanner.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+                if(j==n){
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
